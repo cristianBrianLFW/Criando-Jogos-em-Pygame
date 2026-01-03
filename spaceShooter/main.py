f0a01bb1f393bcef0ec3ffc5fda94fcd3ce3872c
cristian brian <lloydfronteraeng@gmail.com>
2026-01-03 18:45:43 -0300

    Subindo atualizacoes

--- spaceShooter/main.py
+++ spaceShooter/main.py
@@ -4,9 +4,15 @@ import pygame
 
 pygame.init()
 
+pygame.display.set_caption("Space Shooter")
+
+surf = pygame.Surface ((100, 200))
+
+surf.fill ("black")
+
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 
-screen = pygame.display.set_mode ((WINDOW_WIDTH, WINDOW_HEIGHT))
+displaySurface = pygame.display.set_mode ((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 running = True
 
@@ -15,7 +21,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill ("blue")
+    displaySurface.fill ("darkgray")
+    displaySurface.blit (surf, (640,360))
     pygame.display.update ()
 
 pygame.quit()
